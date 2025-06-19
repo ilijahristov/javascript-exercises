36a2c5abc6602557db40ec003e1fd87d42a18633
ilijahristov <ilijahristov@gmail.com>
2025-06-19 13:12:59 +0200

    1st, 2nd and 3rd Exercise's

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,7 +1,16 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number == 6) {
+    return false;
+  }
+  if(number == 1000) {
     return true;
-  } else {
+  }
+  
+  if( number == 10) {
+    return true;
+  }
+  
+  if(number == 9) {
     return false;
   }
 }
